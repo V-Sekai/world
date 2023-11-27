291fed432b7e5d713f00ce15ce714bccead98c4b
K. S. Ernest (iFire) Lee~ <ernest.lee@chibifire.com>
2023-11-26 21:27:09 -0800

    git subrepo pull (merge) --force --branch=groups-4.2 godot
    subrepo:
      subdir:   "godot"
      merged:   "db799f4cb44"
    upstream:
      origin:   "https://github.com/V-Sekai/godot.git"
      branch:   "groups-4.2"
      commit:   "db799f4cb44"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- godot/modules/mvsqlite/config.py
+++ godot/modules/mvsqlite/config.py
@@ -7,7 +7,7 @@ import os
 def can_build(env, platform):
     if platform == "ios" or platform == "web" or platform == "android":
         return False
-    if platform == "windows":
+    if platform == "windows" and not env["use_mingw"]:
         return False
     try:
         subprocess.check_output(["rustup", "--version"], stderr=subprocess.STDOUT)
