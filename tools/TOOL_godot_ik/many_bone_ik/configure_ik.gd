30f404530156ff7e4449df8dc04a48a9bba0ea46
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-02-04 09:56:24 -0800

    Remove pet code.

--- tools/TOOL_godot_ik/many_bone_ik/configure_ik.gd
+++ tools/TOOL_godot_ik/many_bone_ik/configure_ik.gd
@@ -1,36 +1,6 @@
 @tool
 extends EditorScript
 
-func euclidean_distance(p1, p2):
-	return p1.distance_to(p2)
-
-func chamfer_distance(set_A, set_B):
-	var total_distance = 0.0
-
-	for point_A in set_A:
-		var min_distance = INF
-		for point_B in set_B:
-			var distance = euclidean_distance(point_A, point_B)
-			if distance < min_distance:
-				min_distance = distance
-		total_distance += min_distance
-
-	for point_B in set_B:
-		var min_distance = INF
-		for point_A in set_A:
-			var distance = euclidean_distance(point_B, point_A)
-			if distance < min_distance:
-				min_distance = distance
-		total_distance += min_distance
-
-	return total_distance / (set_A.size() + set_B.size())
-
-
-func test_chamfer_distance():
-	var set_A = [Vector2(1, 2), Vector2(3, 4), Vector2(5, 6)]
-	var set_B = [Vector2(7, 8), Vector2(9, 10), Vector2(11, 12)]
-	print(chamfer_distance(set_A, set_B))
-
 const ik_config = preload("res://many_bone_ik/ik_config.gd")
 
 class BoneConstraint:
@@ -46,6 +16,21 @@ class BoneConstraint:
 		self.resistance = resistance
 
 var bone_names = ["Root", "Hips", "Spine", "Chest", "UpperChest", "Neck", "Head", "LeftUpperLeg", "RightUpperLeg", "LeftLowerLeg", "RightLowerLeg", "LeftFoot", "RightFoot", "LeftShoulder", "RightShoulder", "LeftUpperArm", "RightUpperArm", "LeftLowerArm", "RightLowerArm", "LeftHand", "RightHand"]
+
+var bone_constraints: Dictionary
+
+func set_bone_constraint(many_bone_ik: ManyBoneIK3D, p_bone_name: String, p_twist_from: float, p_twist_range: float, p_kususdama: Array):
+	bone_constraints[p_bone_name] = BoneConstraint.new(p_twist_from, p_twist_range, p_kususdama)
+	var constraint_i = many_bone_ik.get_constraint_count()
+	many_bone_ik.set_constraint_count(many_bone_ik.get_constraint_count() + 1)
+	many_bone_ik.set_constraint_name(constraint_i, p_bone_name)
+	many_bone_ik.set_kusudama_twist(constraint_i, Vector2(p_twist_from, p_twist_range))
+	many_bone_ik.set_kusudama_limit_cone_count(constraint_i, 0)
+	many_bone_ik.set_kusudama_limit_cone_count(constraint_i, p_kususdama.size())
+	for cone_constraint_i: int in range(p_kususdama.size()):
+		var cone_constraint = p_kususdama[cone_constraint_i]
+		many_bone_ik.set_kusudama_limit_cone_center(constraint_i, cone_constraint_i, cone_constraint.direction)
+		many_bone_ik.set_kusudama_limit_cone_radius(constraint_i, cone_constraint_i, cone_constraint.limit_angle)
 
 func _run():
 	var root: Node = get_scene()
@@ -86,39 +71,11 @@ func _run():
 			twist_range = config["twist_range"]
 		set_bone_constraint(many_bone_ik, bone_name, twist_from, twist_range, kususdama)
 
-	#many_bone_ik.queue_print_skeleton()
-
 	var bones: Array = [
 		"Root",
 		"Hips",
-		#"Hips",
-		#"Chest",
-		#"LeftLowerArm",
 		"LeftHand",
-		#"LeftThumbProximal",
-		#"LeftIndexProximal",
-		#"LeftMiddleProximal",
-		#"LeftRingProximal",
-		#"LeftLittleProximal",
-		#"LeftThumbDistal",
-		#"LeftIndexDistal",f
-		#"LeftMiddleDistal",
-		#"LeftRingDistal",
-		#"LeftLittleDistal",
-		#"RightLowerArm",
 		"RightHand",
-		#"RightThumbProximal",
-		#"RightIndexProximal",
-		#"RightMiddleProximal",
-		#"RightRingProximal",
-		#"RightLittleProximal",
-		#"RightThumbDistal",
-		#"RightIndexDistal",
-		#"RightMiddleDistal",
-		#"RightRingDistal",
-		#"RightLittleDistal",
-		#"LeftLowerLeg",
-		#"RightLowerLeg",
 		"LeftFoot",
 		"RightFoot",
 		"Head",
@@ -155,15 +112,6 @@ func _run():
 		if bone_name in ["LeftFoot", "RightFoot"]:
 			pose = pose.rotated(Vector3(1, 0, 0), -deg_to_rad(90))
 		targets_3d.global_transform = pose
-		#var label_3d = preload("label_3d.gd").new()
-		#label_3d.skeleton = skeleton
-		#label_3d.font_size = 4
-		#label_3d.no_depth_test = true
-		#label_3d.bone_name = bone_name
-		#label_3d.billboard = BaseMaterial3D.BILLBOARD_FIXED_Y
-		#targets_3d.add_child(label_3d, true)
-		#label_3d.owner = targets_3d.owner
-
 
 	for pin_i in range(bones.size()):
 		var bone_name = bones[pin_i]
@@ -175,44 +123,3 @@ func _run():
 
 	var set_a: Array[Vector3] = []
 	var set_b: Array[Vector3] = []
-
-	#var reference_skeleton = many_bone_ik.owner.get_node("vrm_1_vsekai_godot_engine_humanoid_08/Root/Skeleton3D")
-	#for bone_name in bones:
-		#var bone_id = reference_skeleton.find_bone(bone_name)
-		#if bone_id != -1:
-			#var bone_position = reference_skeleton.get_bone_global_pose(bone_id).origin
-			#set_a.append(bone_position)
-		#else:
-			#print("Bone '%s' not found!" % bone_name)
-	#
-	#var target_skeleton = many_bone_ik.owner.get_node("VVVV_200502/Armature/GeneralSkeleton")
-	#for bone_name in bones:
-		#var bone_id = target_skeleton.find_bone(bone_name)
-		#if bone_id != -1:
-			#var bone_position = target_skeleton.get_bone_global_pose(bone_id).origin
-			#set_b.append(bone_position)
-		#else:
-			#print("Bone '%s' not found!" % bone_name)
-	#var distance = chamfer_distance(set_a, set_b)
-	#print(distance)
-
-var bone_constraints: Dictionary
-
-func get_bone_constraint(p_bone_name: String) -> BoneConstraint:
-	if bone_constraints.has(p_bone_name):
-		return bone_constraints[p_bone_name]
-	else:
-		return BoneConstraint.new()
-
-func set_bone_constraint(many_bone_ik: ManyBoneIK3D, p_bone_name: String, p_twist_from: float, p_twist_range: float, p_kususdama: Array):
-	bone_constraints[p_bone_name] = BoneConstraint.new(p_twist_from, p_twist_range, p_kususdama)
-	var constraint_i = many_bone_ik.get_constraint_count()
-	many_bone_ik.set_constraint_count(many_bone_ik.get_constraint_count() + 1)
-	many_bone_ik.set_constraint_name(constraint_i, p_bone_name)
-	many_bone_ik.set_kusudama_twist(constraint_i, Vector2(p_twist_from, p_twist_range))
-	many_bone_ik.set_kusudama_limit_cone_count(constraint_i, 0)
-	many_bone_ik.set_kusudama_limit_cone_count(constraint_i, p_kususdama.size())
-	for cone_constraint_i: int in range(p_kususdama.size()):
-		var cone_constraint = p_kususdama[cone_constraint_i]
-		many_bone_ik.set_kusudama_limit_cone_center(constraint_i, cone_constraint_i, cone_constraint.direction)
-		many_bone_ik.set_kusudama_limit_cone_radius(constraint_i, cone_constraint_i, cone_constraint.limit_angle)
