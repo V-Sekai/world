bf39343b33fa4c4fc39bc33047707068e80bcaff
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-03-11 14:20:07 -0700

    Fix quaternion normalization and sign inversion
    Adjusted quaternion calculation for proper normalization and fixed sign inversion issue.

--- VSK_godot/modules/many_bone_ik/src/math/qcp.cpp
+++ VSK_godot/modules/many_bone_ik/src/math/qcp.cpp
@@ -116,10 +116,9 @@ Quaternion QCP::calculate_rotation() {
 			quaternion_x /= min;
 			quaternion_y /= min;
 			quaternion_z /= min;
-			result = Quaternion(quaternion_x, quaternion_y, quaternion_z, quaternion_w).normalized();
+			result = Quaternion(quaternion_x, quaternion_y, quaternion_z, -quaternion_w).normalized();
 		}
 	}
-	result.w *= -1;
 
 	return result;
 }
