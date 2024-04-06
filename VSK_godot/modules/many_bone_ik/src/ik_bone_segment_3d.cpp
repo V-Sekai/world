fc9ccf064c7021ebcc8f7dfa51b2c2c9a4328aed
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-04-06 15:20:34 -0700

    git subrepo pull (merge) VSK_godot
    subrepo:
      subdir:   "VSK_godot"
      merged:   "d89e312b1f"
    upstream:
      origin:   "https://github.com/V-Sekai/godot.git"
      branch:   "groups-staging-4.3"
      commit:   "def16b2d2b"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- VSK_godot/modules/many_bone_ik/src/ik_bone_segment_3d.cpp
+++ VSK_godot/modules/many_bone_ik/src/ik_bone_segment_3d.cpp
@@ -30,16 +30,16 @@
 
 #include "ik_bone_segment_3d.h"
 
+#include "core/string/string_builder.h"
 #include "ik_effector_3d.h"
 #include "ik_kusudama_3d.h"
-#include "ik_limit_cone_3d.h"
 #include "many_bone_ik_3d.h"
-#include "math/ik_node_3d.h"
 #include "scene/3d/skeleton_3d.h"
 
 Ref<IKBone3D> IKBoneSegment3D::get_root() const {
 	return root;
 }
+
 Ref<IKBone3D> IKBoneSegment3D::get_tip() const {
 	return tip;
 }
@@ -431,7 +431,15 @@ void IKBoneSegment3D::_finalize_segment(Ref<IKBone3D> p_current_tip) {
 		_enable_pinned_descendants();
 	}
 
-	set_name(vformat("IKBoneSegment%sRoot%sTip", root->get_name(), tip->get_name()));
+	StringBuilder name_builder;
+	name_builder.append("IKBoneSegment");
+	name_builder.append(root->get_name());
+	name_builder.append("Root");
+	name_builder.append(tip->get_name());
+	name_builder.append("Tip");
+
+	String ik_bone_name = name_builder.as_string();
+	set_name(ik_bone_name);
 	bones.clear();
 	create_bone_list(bones, false);
 }
