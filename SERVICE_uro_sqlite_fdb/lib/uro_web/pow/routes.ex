de0c2d55025a330d9878559c4687f8d446806e87
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-04-09 16:05:21 -0700

    git subrepo clone --branch=sqlite-02 https://github.com/V-Sekai/uro.git SERVICE_uro_sqlite_fdb
    subrepo:
      subdir:   "SERVICE_uro_sqlite_fdb"
      merged:   "c4b87c6ef3"
    upstream:
      origin:   "https://github.com/V-Sekai/uro.git"
      branch:   "sqlite-02"
      commit:   "c4b87c6ef3"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

---
+++ SERVICE_uro_sqlite_fdb/lib/uro_web/pow/routes.ex
@@ -0,0 +1,3 @@
+defmodule UroWeb.Pow.Routes do
+  use Pow.Phoenix.Routes
+end
---
+++ SERVICE_uro_sqlite_fdb/priv/repo/migrations/20200514192900_create_users.exs
@@ -0,0 +1,30 @@
+defmodule Uro.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false, options: "STRICT, WITHOUT ROWID") do
+      add :id, :uuid, primary_key: true
+      add :email, :string, null: false
+      add :password_hash, :string
+
+      add :username, :string
+      add :display_name, :string
+
+      add :email_notifications, :boolean, default: false
+
+      add :profile_picture, :string
+
+      add :email_confirmation_token, :string
+      add :email_confirmed_at, :utc_datetime
+      add :unconfirmed_email, :string
+
+      add :locked_at, :utc_datetime
+
+      timestamps()
+    end
+
+    create unique_index(:users, :username)
+    create unique_index(:users, :email)
+    create unique_index(:users, :email_confirmation_token)
+  end
+end
