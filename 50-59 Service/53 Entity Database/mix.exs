facb6604a4a8d6134647a2467dd65fd0decedf53
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-05-29 00:55:13 -0700

    Add libtorch.

--- 50-59 Service/53 Entity Database/mix.exs
+++ 50-59 Service/53 Entity Database/mix.exs
@@ -5,7 +5,7 @@ defmodule EntityDatabase.MixProject do
     [
       app: :entity_database,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
     ]
