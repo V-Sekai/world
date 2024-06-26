964c032a14bb8f5b37ad3fd3aea73e1260ee10c6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-25 21:45:26 -0700

    Added wave function collapse algorithm for improv.

---
+++ 14_planner/tests/test_improv.gd
@@ -0,0 +1,42 @@
+extends "res://addons/gut/test.gd"
+
+var wfc: RefCounted
+
+func before_each():
+	wfc = load("res://improv.gd").new()
+
+func test_calculate_entropy():
+	var square = {"possible_tiles": ["A", "B", "C"]}
+	assert_eq(wfc._calculate_entropy(square), 3)
+
+func test_find_lowest_entropy_square():
+	var state = {
+		"1": {"possible_tiles": ["A", "B", "C"]},
+		"2": {"possible_tiles": ["A", "B"]},
+		"3": {"possible_tiles": ["A"]}
+	}
+	assert_eq(wfc._find_lowest_entropy_square(state), "2")
+
+func test_array_difference():
+	var a1 = ["A", "B", "C"]
+	var a2 = ["B"]
+	assert_eq(wfc.array_difference(a1, a2), ["A", "C"])
+
+func test_find_plan():
+	var state = {}
+	for i in range(6):
+		var possible_tiles = []
+		for graph in wfc.possible_types["@graph"]:
+			possible_tiles.append(graph["@id"])
+		state[i] = { "tile": null, "possible_tiles": possible_tiles }
+	var wfc_array: Array
+	wfc_array.append(["meta_collapse_wave_function"])
+	var planner = Plan.new()
+	planner.current_domain = wfc
+	planner.verbose = 0
+	var result = planner.find_plan(state, wfc_array)
+	var is_valid = wfc.is_valid_sequence(state)
+	assert_true(is_valid, "The sequence is valid.")
+	gut.p(result)
+	assert_eq_deep(result, [["set_tile_state", 0, "[name]"], ["set_tile_state", 1, ": I have a"], ["set_tile_state", 2, "[:animal]"], ["set_tile_state", 3, "who is"], ["set_tile_state", 4, "[years]"], ["set_tile_state", 5, "years old."]])
+	
