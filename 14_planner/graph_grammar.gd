497856c22061b3c70dffb3628f7b280f82af8ee4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-07-01 16:33:20 -0700

    Refactored GraphGrammar class and related methods

--- 14_planner/graph_grammar.gd
+++ 14_planner/graph_grammar.gd
@@ -15,31 +15,30 @@ extends RefCounted
 ## A production rule is defined as `X -> (D, C)`, where `X` is a nonterminal symbol that belongs to the set difference of `Λ` and `Ξ`, `D` is a graph over `Λ` and `Σ`, and `C` is a subset of the Cartesian product of `Λ`, `Λ`, `V(D)`, and `fin; outg`.
 
 class GraphGrammar:
-	const CONTEXT = {
+	@export var CONTEXT = {
 		"gg": "http://v-sekai.com/graphgrammar#",
 		"ex": "http://v-sekai.com/ex#"
 	}
-	var id: String = "ex:myGraphGrammar"
-	var type: String = "gg:GraphGrammar"
-	var initialized: bool = false
-
+	@export var id: String = "ex:myGraphGrammar"
+	@export var type: String = "gg:GraphGrammar"
+	@export var initialized: bool = false
 	class ProductionRule:
-		var id: String
-		var type: String
-		var left_hand_side: String
-		var right_hand_side: Array
+		@export var id: String
+		@export var type: String
+		@export var left_hand_side: String
+		@export var right_hand_side: Array
 		func _init(_id: String, _type: String, _left_hand_side: String, _right_hand_side: Array):
 			self.id = _id
 			self.type = _type
 			self.left_hand_side = _left_hand_side
 			self.right_hand_side = _right_hand_side
 
-	var node_labels: PackedStringArray
-	var terminal_node_labels: PackedStringArray
-	var edge_labels: PackedStringArray
-	var final_edge_labels: PackedStringArray
-	var production_rules: Array[ProductionRule]
-	var initial_nonterminal_symbol: String
+	@export var node_labels: PackedStringArray
+	@export var terminal_node_labels: PackedStringArray
+	@export var edge_labels: PackedStringArray
+	@export var final_edge_labels: PackedStringArray
+	@export var production_rules: Array
+	@export var initial_nonterminal_symbol: String
 	func all_in_array(subset: PackedStringArray, set: PackedStringArray) -> bool:
 		var set_dict = {}
 		for element in set:
@@ -59,7 +58,6 @@ class GraphGrammar:
 		if _terminal_node_labels.has(_initial_nonterminal_symbol):
 			push_error("The initial nonterminal symbol must not be a terminal node label.")
 			return
-
 		for rule in _production_rules:
 			if !_node_labels.has(rule.left_hand_side):
 				push_error("The left-hand side of each production rule must be a possible node label.")
@@ -71,7 +69,6 @@ class GraphGrammar:
 				if !_edge_labels.has(rhs["edge"]):
 					push_error("Each edge in the right-hand side of each production rule must be a possible edge label.")
 					return
-
 		self.id = _id
 		self.type = _type
 		self.node_labels = _node_labels
@@ -82,42 +79,33 @@ class GraphGrammar:
 		self.initial_nonterminal_symbol = _initial_nonterminal_symbol
 		self.initialized = true
 
-static func plan_to_graph_grammar(todo_list: Array, state: Dictionary) -> Dictionary:
-	var graph_grammar = {
-		"@context": {
-			"gg": "http://v-sekai.com/graphgrammar#",
-			"ex": "http://v-sekai.com/ex#"
-		},
-		"@id": "ex:myGraphGrammar",
-		"@type": "gg:GraphGrammar",
-		"gg:nodeLabels": [],
-		"gg:terminalNodeLabels": [],
-		"gg:edgeLabels": ["next"],
-		"gg:finalEdgeLabels": ["next"],
-		"gg:productionRules": [],
-		"gg:initialNonterminalSymbol": todo_list[0][0]
-	}
+static func plan_to_graph_grammar(todo_list: Array, state: Dictionary) -> GraphGrammar:
+	var node_labels = PackedStringArray()
+	var terminal_node_labels = PackedStringArray()
+	var edge_labels = PackedStringArray(["next"])
+	var final_edge_labels = PackedStringArray(["next"])
+	var production_rules: Array[GraphGrammar.ProductionRule]
+	var initial_nonterminal_symbol = todo_list[0][0]
 
+	var node_labels_dict = {}
 	for i in range(todo_list.size()):
 		var node = todo_list[i][0]
 		var possible_tiles = state[i]["possible_tiles"]
 
 		# Add the node label to the list of all possible node labels
-		if !graph_grammar["gg:nodeLabels"].has(node):
-			graph_grammar["gg:nodeLabels"].append(node)
+		if !node_labels_dict.has(node):
+			node_labels.append(node)
+			node_labels_dict[node] = true
 
 		# If this is the last node in the todo list, add it to the list of terminal node labels
 		if i == todo_list.size() - 1:
-			graph_grammar["gg:terminalNodeLabels"].append(node)
+			terminal_node_labels.append(node)
 
 		# For each possible tile, create a production rule from the current node to that tile
 		for tile in possible_tiles:
-			var rule = {
-				"@id": "ex:rule" + str(i),
-				"@type": "gg:Rule",
-				"gg:leftHandSide": node,
-				"gg:rightHandSide": [{"node": tile, "edge": "next"}]
-			}
-			graph_grammar["gg:productionRules"].append(rule)
+			var rule = GraphGrammar.ProductionRule.new("ex:rule" + str(i), "gg:Rule", node, [{"node": tile, "edge": "next"}])
+			production_rules.append(rule)
+
+	var graph_grammar = GraphGrammar.new("ex:myGraphGrammar", "gg:GraphGrammar", node_labels, terminal_node_labels, edge_labels, final_edge_labels, production_rules, initial_nonterminal_symbol)
 
 	return graph_grammar
