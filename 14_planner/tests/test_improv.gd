76f1bf1614a0b6a73820b41d9e4683d0f89f91ca
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-07-01 10:47:42 -0700

    Add a plan to graph grammar converter.

--- 14_planner/tests/test_improv.gd
+++ 14_planner/tests/test_improv.gd
@@ -21,13 +21,59 @@ func test_find_plan():
 		for graph in wfc.possible_types["gg:nodeLabels"]:
 			possible_tiles.append(graph)
 		state[i] = { "tile": null, "possible_tiles": possible_tiles }
-	var wfc_array: Array = []
-	wfc_array.append(["meta_collapse_wave_function"])
+	var todo_list: Array = [["meta_collapse_wave_function"]]
 	var planner = Plan.new()
 	planner.current_domain = wfc
 	planner.verbose = 1
-	var result = planner.find_plan(state, wfc_array)
+	gut.p(todo_list)
+	gut.p(state)
+	var graph_grammar = plan_to_graph_grammar(todo_list, state)
+	gut.p(graph_grammar)
+	var result = planner.find_plan(state, todo_list)
 	#var is_valid = wfc.is_valid_sequence(state)
 	#assert_true(is_valid, "The sequence is valid.")
 	gut.p(result)
-	assert_eq_deep(result, [["set_tile_state", 0, "Bob"], ["set_tile_state", 1, ": I have a"], ["set_tile_state", 2, "parrot"], ["set_tile_state", 3, "who is"], ["set_tile_state", 4, "7 years old."]])
+	gut.p(state)
+	graph_grammar = plan_to_graph_grammar(result, state)
+	gut.p(graph_grammar)
+	assert_eq_deep(result, [["set_tile_state", 0, "root"], ["set_tile_state", 1, "Bob"], ["set_tile_state", 2, ": I have a"], ["set_tile_state", 3, "dog"], ["set_tile_state", 4, "who is"], ["set_tile_state", 5, "2 years old."], ["set_tile_state", 6, "end"]])
+
+func plan_to_graph_grammar(todo_list: Array, state: Dictionary) -> Dictionary:
+	var graph_grammar = {
+		"@context": {
+			"gg": "http://v-sekai.com/graphgrammar#",
+			"ex": "http://v-sekai.com/ex#"
+		},
+		"@id": "ex:myGraphGrammar",
+		"@type": "gg:GraphGrammar",
+		"gg:nodeLabels": [],
+		"gg:terminalNodeLabels": [],
+		"gg:edgeLabels": ["next"],
+		"gg:finalEdgeLabels": ["next"],
+		"gg:productionRules": [],
+		"gg:initialNonterminalSymbol": todo_list[0][0]
+	}
+
+	for i in range(todo_list.size()):
+		var node = todo_list[i][0]
+		var possible_tiles = state[i]["possible_tiles"]
+
+		# Add the node label to the list of all possible node labels
+		if !graph_grammar["gg:nodeLabels"].has(node):
+			graph_grammar["gg:nodeLabels"].append(node)
+
+		# If this is the last node in the todo list, add it to the list of terminal node labels
+		if i == todo_list.size() - 1:
+			graph_grammar["gg:terminalNodeLabels"].append(node)
+
+		# For each possible tile, create a production rule from the current node to that tile
+		for tile in possible_tiles:
+			var rule = {
+				"@id": "ex:rule" + str(i),
+				"@type": "gg:Rule",
+				"gg:leftHandSide": node,
+				"gg:rightHandSide": [{"node": tile, "edge": "next"}]
+			}
+			graph_grammar["gg:productionRules"].append(rule)
+
+	return graph_grammar
