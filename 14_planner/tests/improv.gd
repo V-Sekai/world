40d0e0a230832d215bd75b97fb846b57bc5c7e66
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-07-01 18:34:52 -0700

    Cleanup the code.

--- 14_planner/tests/improv.gd
+++ 14_planner/tests/improv.gd
@@ -4,39 +4,12 @@ class_name Improv
 
 # https://robertheaton.com/2018/12/17/wavefunction-collapse-algorithm/
 
-@export var possible_types = null
+@export var possible_types: GraphGrammar = null
 
 func _init() -> void:
 	add_actions([set_tile_state, remove_possible_tiles])
 	add_task_methods("collapse_wave_function", [collapse_wave_function])
 	add_task_methods("meta_collapse_wave_function", [meta_collapse_wave_function])
-	var production_rules: Array[GraphGrammar.ProductionRule] = [
-		GraphGrammar.ProductionRule.new("ex:rule1", "gg:Rule", "root", [{"node": "Bob", "edge": "next"}, {"node": "Alice", "edge": "next"}, {"node": "Carol", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule2", "gg:Rule", "Bob", [{"node": ": I have a", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule3", "gg:Rule", "Alice", [{"node": ": I have a", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule4", "gg:Rule", "Carol", [{"node": ": I have a", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule5", "gg:Rule", ": I have a", [{"node": "dog", "edge": "next"}, {"node": "cat", "edge": "next"}, {"node": "parrot", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule6", "gg:Rule", "dog", [{"node": "who is", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule7", "gg:Rule", "cat", [{"node": "who is", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule8", "gg:Rule", "parrot", [{"node": "who is", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule9", "gg:Rule", "who is", [{"node": "2 years old.", "edge": "next"}, {"node": "3 years old.", "edge": "next"}, {"node": "4 years old.", "edge": "next"}, {"node": "5 years old.", "edge": "next"}, {"node": "6 years old.", "edge": "next"}, {"node": "7 years old.", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule10", "gg:Rule", "2 years old.", [{"node": "end", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule11", "gg:Rule", "3 years old.", [{"node": "end", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule12", "gg:Rule", "4 years old.", [{"node": "end", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule13", "gg:Rule", "5 years old.", [{"node": "end", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule14", "gg:Rule", "6 years old.", [{"node": "end", "edge": "next"}]),
-		GraphGrammar.ProductionRule.new("ex:rule15", "gg:Rule", "7 years old.", [{"node": "end", "edge": "next"}])
-	]
-	possible_types = GraphGrammar.new(
-		"ex:myGraphGrammar", 
-		"gg:GraphGrammar", 
-		["root", "Bob", "Alice", "Carol", ": I have a", "dog", "cat", "parrot", "who is", "2 years old.", "3 years old.", "4 years old.", "5 years old.", "6 years old.", "7 years old.", "end"], 
-		["end"], 
-		["next"], 
-		["next"], 
-		production_rules,
-		"root"
-	)
 	
 # Function to calculate entropy of a square
 static func _calculate_entropy(square) -> int:
--- 14_planner/tests/test_improv.gd
+++ 14_planner/tests/test_improv.gd
@@ -12,14 +12,41 @@ func test_array_difference():
 func test_find_plan():
 	var state = {}
 	var improv: Improv = Improv.new()
+	var planner = Plan.new()
+	planner.current_domain = Improv.new()
+	var production_rules: Array[GraphGrammar.ProductionRule] = [
+		GraphGrammar.ProductionRule.new("ex:rule1", "gg:Rule", "root", [{"node": "Bob", "edge": "next"}, {"node": "Alice", "edge": "next"}, {"node": "Carol", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule2", "gg:Rule", "Bob", [{"node": ": I have a", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule3", "gg:Rule", "Alice", [{"node": ": I have a", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule4", "gg:Rule", "Carol", [{"node": ": I have a", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule5", "gg:Rule", ": I have a", [{"node": "dog", "edge": "next"}, {"node": "cat", "edge": "next"}, {"node": "parrot", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule6", "gg:Rule", "dog", [{"node": "who is", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule7", "gg:Rule", "cat", [{"node": "who is", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule8", "gg:Rule", "parrot", [{"node": "who is", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule9", "gg:Rule", "who is", [{"node": "2 years old.", "edge": "next"}, {"node": "3 years old.", "edge": "next"}, {"node": "4 years old.", "edge": "next"}, {"node": "5 years old.", "edge": "next"}, {"node": "6 years old.", "edge": "next"}, {"node": "7 years old.", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule10", "gg:Rule", "2 years old.", [{"node": "end", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule11", "gg:Rule", "3 years old.", [{"node": "end", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule12", "gg:Rule", "4 years old.", [{"node": "end", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule13", "gg:Rule", "5 years old.", [{"node": "end", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule14", "gg:Rule", "6 years old.", [{"node": "end", "edge": "next"}]),
+		GraphGrammar.ProductionRule.new("ex:rule15", "gg:Rule", "7 years old.", [{"node": "end", "edge": "next"}])
+	]
+	planner.current_domain.possible_types = GraphGrammar.new(
+		"ex:myGraphGrammar", 
+		"gg:GraphGrammar", 
+		["root", "Bob", "Alice", "Carol", ": I have a", "dog", "cat", "parrot", "who is", "2 years old.", "3 years old.", "4 years old.", "5 years old.", "6 years old.", "7 years old.", "end"], 
+		["end"], 
+		["next"], 
+		["next"], 
+		production_rules,
+		"root"
+	)
 	for i in range(7):
 		var possible_tiles = []
-		for graph in improv.possible_types.node_labels:
+		for graph in planner.current_domain.possible_types.node_labels:
 			possible_tiles.append(graph)
 		state[i] = { "tile": null, "possible_tiles": possible_tiles }
 	var todo_list: Array = [["meta_collapse_wave_function"]]
-	var planner = Plan.new()
-	planner.current_domain = Improv.new()
 	planner.verbose = 0
 	gut.p("Todo list: ")
 	gut.p(todo_list)
