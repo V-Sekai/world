b9ec3ec64b6f1795eaa3f6aec65e54aabeb0ff48
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-07-04 08:34:13 -0700

    Make a planner.

---
+++ 14_planner/Central/Developers/Forest/Behave.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@onready var strategic_plan = preload ("res://path_to/StrategicPlan.gd").new()
+@onready var tactical_plan = preload ("res://path_to/TacticalPlan.gd").new()
+@onready var operational_plan = preload ("res://path_to/OperationalPlan.gd").new()
+
+func _ready():
+    # Connect signals between planners
+    strategic_plan.connect("goal_selected", tactical_plan, "_on_goal_selected")
+    tactical_plan.connect("task_selected", operational_plan, "_on_task_selected")
+
+    # Example memory dictionary
+    var memory = {
+        "firepit_has_fuel": {"firepit": false},
+        "hunger_level": {"satyr": "high"},
+        "is_afraid": {"satyr": true}
+    }
+
+    # Offload the behavior process to worker threads
+    var strategic_task_id = WorkerThreadPool.add_task(strategic_plan.behave, memory)
+    WorkerThreadPool.wait_for_task_completion(strategic_task_id)
+
+    var tactical_task_id = WorkerThreadPool.add_task(tactical_plan._on_goal_selected, tactical_plan.selected_goal)
+    WorkerThreadPool.wait_for_task_completion(tactical_task_id)
+
+    var operational_task_id = WorkerThreadPool.add_task(operational_plan._on_task_selected, operational_plan.selected_task)
+    WorkerThreadPool.wait_for_task_completion(operational_task_id)
